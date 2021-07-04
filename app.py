e1a2d8c170ca06f9cb713468e8c83b122b0f54e8
Filipe Fernandes <filipelopesfernandes@gmail.com>
2021-07-04 20:23:37 +0100

    Flask deployed and working

---
+++ app.py
@@ -0,0 +1,46 @@
+import base64
+import io
+import os
+import sys
+import cv2
+import numpy as np
+from PIL import Image
+
+from detect import run
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('./index.html')
+
+
+@app.route('/detectObject', methods=['POST'])
+def mask_image():
+	# print(request.files , file=sys.stderr)
+    file = request.files['image'].read()
+    npimg = np.fromstring(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    cv2.imwrite("../data/images/image.png",img)
+
+    run(weights="../yolov5s.pt", source="../data/images/",project="../results", name="",exist_ok=True)
+
+    imageResults = cv2.imread("../results/image.png",cv2.IMREAD_COLOR)
+    is_sucess, bufImg = cv2.imencode(".png",imageResults)
+    bytesImage = bufImg.tobytes()
+    img_base64 = base64.b64encode(bytesImage)
+
+    return jsonify({'status':str(img_base64)})
+
+@app.after_request
+def after_request(response):
+    print("log: setting cors" , file = sys.stderr)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+if __name__ == '__main__':
+	app.run(debug = True)
---
+++ static/index.js
@@ -0,0 +1,54 @@
+var readytosend = false
+
+window.onload = () => {
+	$('#sendbutton').click(() => {
+		imagebox = $('#imagebox')
+		input = $('#imageinput')[0]
+		if(readytosend)
+		{
+			let formData = new FormData();
+			formData.append('image' , input.files[0]);
+			$.ajax({
+				url: "http://localhost:5000/detectObject", // fix this to your liking
+				type:"POST",
+				data: formData,
+				cache: false,
+				processData:false,
+				contentType:false,
+				error: function(data){
+					console.log("upload error" , data);
+					console.log(data.getAllResponseHeaders());
+				},
+				success: function(data){
+					console.log(data);
+					bytestring = data['status']
+					image = bytestring.split('\'')[1]
+					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
+				}
+			});
+		}
+	});
+};
+
+function readUrl(input){
+	imagebox = $('#imagebox')
+	console.log("evoked readUrl")
+	console.log(input.files)
+	if(input.files && input.files[0]){
+		filename = input.files[0].name.split(".")
+		if(filename[1] == "jpg" || filename == "png"){
+			let reader = new FileReader();
+		    reader.onload = function(e){
+			console.log(e)
+			imagebox.attr('src',e.target.result); 
+			imagebox.height(500);
+			imagebox.width(800);
+			}
+			reader.readAsDataURL(input.files[0]);
+			readytosend = true
+		}else{
+			console.log("error")
+			readytosend = false
+		}
+	}	
+}
