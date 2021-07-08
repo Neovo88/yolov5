90403a50c4fffe94c7e765799d22012d3c6995b7
Filipe Fernandes <filipelopesfernandes@gmail.com>
2021-07-09 00:13:15 +0100

    Created batch for installs

---
+++ start.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+pip install flask
+pip install opencv-python
+pip install pillow
+pip install torch
+pip install pandas
+pip install requests
+pip install pyyaml
+pip install tqdm
+pip install torchvision
+pip install matplotlib
+pip install seaborn
+md images
+PAUSE
